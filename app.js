fdae2a981066ed1013f48d48981462acf3ecf2d1
Juan Manuel Pedrozo <juanmapedrozo@gmail.com>
2021-09-20 07:03:02 -0300

    actualizacion

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path');
 app.use(express.static('public'));
 
-//app.get('/', (req,res) => {res.sendFile(path.resolve('./views/home.html'));
-app.get('/',(req,res)=>{ res.sendFile(path.resolve('./views/login.html'))});
-//app.get('/', (req, res) => { res.sendFile(path.resolve('./views/register.html')) });
+app.get('/', (req,res) => {res.sendFile(path.resolve('./views/home.html'));});
+app.get('/',(req,res)=>{ res.sendFile(path.resolve('./views/login.html'));});
+app.get('/', (req, res) => { res.sendFile(path.resolve('./views/register.html')) ;});
 
-//});
+
 
 app.listen(process.env.PORT || 8080, () => console.log('servidor en puerto 8080'));
