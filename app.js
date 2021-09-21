5e5b4144f53979f9bf1cfd32c554947b534450b6
Juan Manuel Pedrozo <juanmapedrozo@gmail.com>
2021-09-21 14:25:10 -0300

    m

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/login.html',(req,res)=>{ res.sendFile(path.resolve('./views/login.html
 app.get('/register.html', (req, res) => { res.sendFile(path.resolve('./views/register.html')) ;});
 app.get('/home.html', (req,res) => {res.sendFile(path.resolve('./views/home.html'));});
 
+const port = process.env.PORT || 8080;
 
-
-app.listen(process.env.PORT || 8080, () => console.log('servidor en puerto 8080'));
+app.listen(port, () => console.log('servidor en puerto 8080'));
